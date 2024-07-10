9b9c7c107a13275954d08bba0610796b7052513a
aitbaker <157494843+aitbaker@users.noreply.github.com>
2024-07-10 16:10:21 +0200

    Update TestBase.java

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,25 +1,39 @@
 package com.demoqa.tests;
 
+import com.demoqa.config.ApplicationManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     public WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
+    ApplicationManager app = new ApplicationManager(System.getProperty("browser", "chrome"));
 
-        driver.get("https://demoqa.com");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeMethod
+    public void startBrowser(Method method) {
+        logger.info("Start test: " + method.getName());
+        driver = app.startTest();
     }
 
     @AfterMethod(enabled = true)
-    public void tearDown() {driver.quit();}
+    public void tearDown(ITestResult result) {
+        app.stopTest();
+
+        if (result.isSuccess()) {
+            logger.info("Test result: PASSED " + result.getMethod().getMethodName());
+        } else {
+            logger.error("Test result FAILED " + result.getMethod().getMethodName());
+        }
+        logger.info("***********************************************************************");
+    }
+
 }
